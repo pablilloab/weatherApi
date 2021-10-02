644ddec15c14129795a56e001b2fc094ea61dc97
Pablo <pab.a.bari@gmail.com>
2021-10-01 21:07:18 -0300

    Adding documentation on js files

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
+//server api data
 const apiKey = '8eb6e8a7d5ae405db06145440211909'
 const baseURL = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=`
 
-
+//get user location preference
 const button = document.querySelector(".send-location")
 button.addEventListener("click",(event)=>{
     event.preventDefault()
@@ -10,14 +11,14 @@ button.addEventListener("click",(event)=>{
 
 })
 
-
+//get api data from user location
 async function getWeatherData (location) {
     
     let result = await fetch (`${baseURL}${location}`)
             .then(result => result.json())
     console.log(result)
 
-
+    //format report to insert html
     let statusHtml = `<h1>${result.current.temp_c} °C</h1>
     <h1>${result.current.condition.text}</h1>
     <h2>${result.current.wind_kph} km/h</h2>
